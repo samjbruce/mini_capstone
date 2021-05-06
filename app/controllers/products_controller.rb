332e1c865a417747a25080bebb5fc465b01ad307
samjbruce <samueljamesbruce@gmail.com>
2021-05-06 11:21:35 -0500

    Adds routes to display individual products in JSON

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,13 @@ class ProductsController < ApplicationController
   def all_products
     render json: Product.all
   end
+  def reaver
+    render json: Product.find_by(id: 1)
+  end
+  def pipeline
+    render json: Product.find_by(id: 2)
+  end
+  def aquos
+    render json: Product.find_by(id: 3)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/all_products", controller: "products", action: "all_products"
+  get "/reaver", controller: "products", action: "reaver"
+  get "/pipeline", controller: "products", action: "pipeline"
+  get "/aquos", controller: "products", action: "aquos"
 end
