fcbebf1de4e0ec7a43413175ecab5b16c2e5a29f
samjbruce <samueljamesbruce@gmail.com>
2021-05-06 09:28:34 -0500

    Adds a route to display all products in JSON

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "Syndicate Reaver Fly Rod", price: 775, image_uel: "https://cdn11.bigcommerce.com/s-qkggj5v711/images/stencil/1024x1024/products/130/440/Syndicate_Reaver_Full_Rod_white_background-1080x720__41552.1614003811.jpg?c=2", description: "Nymphing fly rod built with nymphing in mind.")
+
+Product.create(name: "Syndicate P2 Pipeline Pro Series", price: 300, image_uel: "https://cdn11.bigcommerce.com/s-qkggj5v711/images/stencil/960w/products/114/380/P21024_V1__75492.1553216309.jpg?c=2", description: "Competition fly rod built to up your euro nymphing game.")
+
+Product.create(name: "Syndicate Aquos Series", price: 375, image_uel: "https://cdn11.bigcommerce.com/s-qkggj5v711/images/stencil/960w/products/115/388/AQ1064_V1__41360.1553216310.jpg?c=2", description: "Great all around rod built to cover a variety of water and species.")
