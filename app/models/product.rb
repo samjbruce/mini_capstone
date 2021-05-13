4716ab4da6334a1e086472468aa4b537a2b96441
samjbruce <samueljamesbruce@gmail.com>
2021-05-13 14:17:55 -0500

    adds validators to model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :price, :inventory, numericality: {greater_than: 0}
+  validates :description, length: {in: 10..500}
+  validates_format_of :image_url, :with => %r{\.(png|jpg|jpeg)$}i, :message => "Image URL must have valid file type", :multiline => true
+
+  
   def is_discounted?
     price < 500
   end
@@ -11,5 +17,4 @@ class Product < ApplicationRecord
     tax + price
   end
 
-  
 end
