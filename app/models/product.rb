dd5fff2d1982e7ceee7537bd79f180019cdc9e6a
samjbruce <samueljamesbruce@gmail.com>
2021-05-24 15:16:29 -0500

    adds has-many through relationships to products and categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
 
   has_many :category_products
+  has_many :categories, through: :category_products
   has_many :orders
   has_many :images
   belongs_to :supplier
